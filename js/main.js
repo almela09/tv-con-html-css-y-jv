e35e2d552ad7b7e8783ec3e109ce7bc7f604b403
almela09 <145797671+almela09@users.noreply.github.com>
2024-02-05 19:15:34 +0100

    probaré switch para cambiar de canales

--- js/main.js
+++ js/main.js
@@ -6,13 +6,21 @@ let arrayButtons = Array.from("btn");
 
 let screen = document.getElementById("screen");
 
-arrayButtons.forEach(item => {
-    item.addEventListener("click", (evento)=>{
-        const currentClass = screen.classList[screen.classList.length -1];
-        if (currentClass.startsWith("canal")){
-            screen.classList.remove(currentClass);
-        }
-        screen.classList.add("canal"+ evento.target.id.slice(-1));
-    });
-});
 
+//Switch para los canales? viable?
+
+switch (canal) {
+    case "canal1":
+
+        break;
+    case "canal2":
+        break;
+    case "canal3":
+        break;
+    case "canal4":
+        break;
+    case "canal5":
+        break;
+    case "canal6":
+        break;
+}
