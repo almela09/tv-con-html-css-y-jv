243818bae203f8e5d4de8035ad0b67245c6c6763
almela09 <145797671+almela09@users.noreply.github.com>
2024-02-05 19:00:41 +0100

    probando js, de momento fail total

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ const buttons = document.getElementsByClassName("btn");
 
 let arrayButtons = Array.from("btn");
 
+let screen = document.getElementById("screen");
+
+arrayButtons.forEach(item => {
+    item.addEventListener("click", (evento)=>{
+        const currentClass = screen.classList[screen.classList.length -1];
+        if (currentClass.startsWith("canal")){
+            screen.classList.remove(currentClass);
+        }
+        screen.classList.add("canal"+ evento.target.id.slice(-1));
+    });
+});
+
