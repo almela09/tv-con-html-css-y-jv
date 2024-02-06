ff1f1481e37daf22a738dc891357533655833c08
almela09 <145797671+almela09@users.noreply.github.com>
2024-02-06 19:57:47 +0100

    cambiar de canal, fecha y numero de la tv. No funciona aún

--- js/main.js
+++ js/main.js
@@ -5,23 +5,30 @@ let tvEncendida = false;
 
 // Toggle
 
-function toggleTv(){
+function toggleTv() {
     tvEncendida = !tvEncendida;
 
     const screen = document.getElementById('screen');
     const canalNumero = document.getElementById('canalNumero');
-    if(tvEncendida){
+    if (tvEncendida) {
         screen.classList.remove('off');
         screen.classList.add('basicScreen');
         canalNumero.textContent = '';
 
-    }else{
-      screen.className = 'basicScreen off';
-      canalNumero.textContent = '';  
+    } else {
+        screen.className = 'basicScreen off';
+        canalNumero.textContent = '';
     }
 }
 
+// Cambiar el canal de tv-fecha-numero canal
+
+function cambiarCanal(canal){
+    if(!tvEncendida) return;
+const screen = document.getElementById('screen');
+const canalNumero = document.getElementById('canalNumero');
+const fecha =new Date().toLocaleDateString();
+}
 
 
-  
 
