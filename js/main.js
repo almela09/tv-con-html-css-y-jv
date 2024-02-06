e4a569efca67b5e55b326acff70b0d9f10f93e53
almela09 <145797671+almela09@users.noreply.github.com>
2024-02-06 21:15:17 +0100

    prueba (otra) de js para cambiar canales, en proceso

--- js/main.js
+++ js/main.js
@@ -24,17 +24,29 @@ function toggleTv() {
 // Cambiar el canal de tv-fecha-numero canal
 //innerHtml = para modificar elementos del html, añadir movidas desde el js.
 
-function cambiarCanal(canal){
-    if(!tvEncendida) return;
-const screen = document.getElementById('screen');
-const canalNumero = document.getElementById('canalNumero');
-const fecha =new Date().toLocaleDateString();
-screen.className = 'basicScreen';
-canalNumero.innerHTML = `Canal ${canal} ${fecha}`;
-screen.classList.add(`canal${canal}`);
+function cambiarCanal(canal) {
+    if (!tvEncendida) return;
+    const screen = document.getElementById('screen');
+    const canalNumero = document.getElementById('canalNumero');
+    const fecha = new Date().toLocaleDateString();
+    screen.className = 'basicScreen';
+    canalNumero.innerHTML = `Canal ${canal} ${fecha}`;
+    screen.classList.add(`canal${canal}`);
 
 }
-document.getElementById('onOff').addEventListener('click',toggleTv);
+document.getElementById('onOff').addEventListener('click', toggleTv);
 
+
+// CANALES CAMBIO
+const canales = document.querySelectorAll('.btn:not(.onOff)');
+
+//.btn:not(.onOff) ----> es una pseudoclase que excluye a los selectores que tienen la clase onOff
+
+canales.forEach((boton)) => {
+    boton.addEventListener('click', function () {
+        if (!tvEncendida) return;
+        const canal =
+    });
+}
 
 
