3314d98fba2549f766ca75750077f2e92d005c7d
almela09 <145797671+almela09@users.noreply.github.com>
2024-02-06 21:46:35 +0100

    funciones básicas proyecto mas o menos hechas y operativas

--- js/main.js
+++ js/main.js
@@ -42,11 +42,17 @@ const canales = document.querySelectorAll('.btn:not(.onOff)');
 
 //.btn:not(.onOff) ----> es una pseudoclase que excluye a los selectores que tienen la clase onOff
 
-canales.forEach((boton)) => {
-    boton.addEventListener('click', function () {
+canales.forEach((boton) => {
+    boton.addEventListener('click', function() {
         if (!tvEncendida) return;
-        const canal =
+        const canal = this.id.replace('canal', '');
+        cambiarCanal(canal);
     });
-}
+});
+
+
+//fallito """"importante""""""": cuando enciende debería quedarse en el canal donde se quedo la última vez.
+
+//volumen si me da tiempo, cosa que dudo.
 
 
