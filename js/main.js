54fa8e764726407faf26ec4e9ba5a09e0be7e739
almela09 <145797671+almela09@users.noreply.github.com>
2024-02-06 16:44:51 +0100

    rehaciendo código porque era un desastre

--- js/main.js
+++ js/main.js
@@ -1,28 +1,6 @@
 
 
-const buttons = document.getElementsByClassName("btn");
-
-let arrayButtons = Array.from("btn");
-
-let screen = document.getElementById("screen");
 
 
-//Switch para los canales? viable?
-
-switch (canal) {
-    case "canal1":
-
-        break;
-    case "canal2":
-        break;
-    case "canal3":
-        break;
-    case "canal4":
-        break;
-    case "canal5":
-        break;
-    case "canal6":
-        break;
-}
-
+  
 
