7cff169ad70af6256639806a69e5f162e2ad1dd5
almela09 <145797671+almela09@users.noreply.github.com>
2024-02-06 19:38:42 +0100

    JS boton on / off en proceso

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 
+// Boton On / Off
+
+let tvEncendida = false;
+
+// Toggle
+
+function toggleTv(){
+    tvEncendida = !tvEncendida;
+
+    const screen = document.getElementById('screen');
+    const canalNumero = document.getElementById('canalNumero');
+   
+}
 
 
 
